e755872112e50460fc31275dd3052eb6b114417a
Victor Williams <victor.williams1030@gmail.com>
2020-05-12 13:49:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,9 @@
 def line(array)
-katz_deli = []
+katz_deli_line = []
   if array.length == 0 
     puts "The line is currently empty."
-  end
-end
-
-def take_a_number(line)
-katz_deli = []
-  line.each_with_index 
+  else 
+    array.each_with_index { |name, index| katz_deli_line.push("#{index + 1}. #{name}")
     puts "The line is currently: #{index + 1}. #{name}"
   end
 end
